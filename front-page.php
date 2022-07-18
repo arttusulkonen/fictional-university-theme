32e5773999200772cd6a5f455fd4967c152d6a87
Arttu <arttu.sulkonen@icloud.com>
2022-07-18 10:24:30 +0300

    install ACF add custom date picker to event posts

--- front-page.php
+++ front-page.php
@@ -20,12 +20,16 @@
                 'post_type' => 'event'
               );
               $query = new WP_Query( $args );
+              
               while ($query-> have_posts() ) {
                 $query->the_post(); ?>
                 <div class="event-summary">
                   <a class="event-summary__date t-center" href="<?php the_permalink();?>">
-                    <span class="event-summary__month"><?php the_time('M')?></span>
-                    <span class="event-summary__day"><?php the_time('d')?></span>
+                    <span class="event-summary__month"><?php  
+                      $eventDate = new DateTime(get_field('event_date'));
+                      echo $eventDate->format('M');
+                    ?></span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('d');?></span>
                   </a>
                   <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
@@ -50,6 +54,7 @@
               $args = array( 
                 'posts_per_page' => 2
               );
+              
               $query = new WP_Query( $args );
               while ($query-> have_posts() ) {
                 $query->the_post(); ?>
