927898febdae39da18a964b67901a77094cc50d9
Arttu <arttu.sulkonen@icloud.com>
2022-07-23 16:17:38 +0300

    add pageBanner function to remaining files

--- functions.php
+++ functions.php
@@ -10,13 +10,14 @@ function pageBanner($args = NULL) {
     $args['subtitle'] = get_field('page_banner_subtitle');
   }
 
-  if(!$args['photo']) {
-    if (get_field('page_banner')) {
+  if (!$args['photo']) {
+    if (get_field('page_banner') AND !is_archive() AND !is_home() ) {
       $args['photo'] = get_field('page_banner')['sizes']['pageBanner'];
     } else {
       $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
     }
   }
+
   ?>
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(
